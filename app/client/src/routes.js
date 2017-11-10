2bd5b9745abf09597217ef9f17eb77f084b2b0cd
Katherine Huang <kat.m.huang@gmail.com>
2017-11-09 19:12:10 -0500

    Add tracksCtrl back to routes

--- app/client/src/routes.js
+++ app/client/src/routes.js
@@ -29,6 +29,7 @@ angular.module('reg')
       .state('tracks', {
         url: "/tracks",
         templateUrl: "views/tracks/tracks.html",
+        controller: 'TracksCtrl',
         resolve: {
           'settings': function(SettingsService){
             return SettingsService.getPublicSettings();
